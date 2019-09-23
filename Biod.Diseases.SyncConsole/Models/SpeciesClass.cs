495f5e460689ae0efe2e9edfcacc5c4c0027493e
Vivian Hu <vivian.hu@bluedot.global>
2019-09-23 14:04:15 +0000

    Merged in feature/pt-82/george-disease-api-schema-change (pull request #1)
    Feature/pt 82/george disease api schema change
    
    * Geoge Disease Api schema changes

---
+++ Biod.Diseases.SyncConsole/Models/SpeciesClass.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Biod.Diseases.SyncConsole.Models
+{
+
+    public class SpeciesClass
+    {
+        public int speciesId { get; set; }
+        public string speciesName { get; set; }
+    }
+
+}
