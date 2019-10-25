8198fe3680ac4abe684be5ee83881b33e7c5dfe2
Predrag Ostojic <predrag.ostojic@bluedot.global>
2019-10-25 19:13:18 +0000

    Merged in hotfix/PT-291-full-text-search (pull request #52)
    Add full text search for Geonames in Surveillance.
    
    * Add full text search for Geonames in Surveillance.
    
        PT-291
    
    * Fix the usp_SearchGeonames SP and misnamed index
    
        PT-291
    
    Approved-by: Basam Ahmad <basam.ahmad@bluedot.global>

---
+++ Biod.Surveillance.Database/place/Views/uvw_AlternateNames.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [place].[uvw_AlternateNames]
+    WITH SCHEMABINDING
+    AS  
+    SELECT        GeonameId, Alternatenames
+    FROM          place.Geonames
+GO
+
+CREATE UNIQUE CLUSTERED INDEX [idx_uvw_AlternateNames_GeonameId]
+    ON [place].[uvw_AlternateNames]([GeonameId] ASC);
+
+GO
