2507184634ad95e5b34086c8985e1452d2bef07c
Anna Caldwell <anna.caldwell@bluedot.global>
2019-10-28 14:13:21 +0000

    Fix issue with suggested events not being deleted
    A suggested event was not being deleted in the db because the parameter of the controller does not match the tag in the data passed by `_SuggestedEventView.cshtml`. The order of operations has also been changed so that only when the suggested event been successfully removed from the db will it be removed from the left panel list.
    
    Resolves PT-366.
    
    Approved-by: Basam Ahmad
    Approved-by: Gursharn Singh
    Approved-by: Sonya Karamchandani

--- Biod.Surveillance/Controllers/HomeController.cs
+++ Biod.Surveillance/Controllers/HomeController.cs
@@ -2315,13 +2315,13 @@ namespace Biod.Surveillance.Controllers
         }
 
         [HttpPost]
-        public string DeleteSuggestedEvent(string eventId)
+        public JsonResult DeleteSuggestedEvent(string eventId)
         {
             var eventItem = dbContext.SuggestedEvents
                 .SingleOrDefault(e => e.SuggestedEventId == eventId);
             if (eventItem == null)
             {
-                return "Event not found";
+                return Json(new { status = "error", message = $"no suggested event exists with ID {eventId}" });
             }
 
             dbContext.SuggestedEvents.Remove(eventItem);
@@ -2335,7 +2335,7 @@ namespace Biod.Surveillance.Controllers
             };
             Task<HttpResponseMessage> response = UpdateUserActiononSuggestedEvent(eventId, userAction);
 
-            return "Deleted";
+            return Json(new { status = "success" });
         }
 
         /// <summary>
