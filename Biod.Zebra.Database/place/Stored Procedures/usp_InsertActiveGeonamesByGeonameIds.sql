e78ffc3a2b5422d335e225463d1c9752ee941ef3
Vivian Hu <vivian.hu@bluedot.global>
2019-10-29 13:07:46 +0000

    Merged in feature/PT-382-modify-usp_insertactivegeonamesby (pull request #63)
    To include new geoname's province and country
    
    * To include new geoname's province and country
    
    Approved-by: Basam Ahmad

--- Biod.Zebra.Database/place/Stored Procedures/usp_InsertActiveGeonamesByGeonameIds.sql
+++ Biod.Zebra.Database/place/Stored Procedures/usp_InsertActiveGeonamesByGeonameIds.sql
@@ -19,6 +19,18 @@ BEGIN
 		Except
 		Select GeonameId
 		From [place].[ActiveGeonames]
+	--add their provinces and countries if not already there
+	Insert into @tbl_geonameIds
+		Select Distinct f2.Admin1GeonameId as GeonameId
+		From @tbl_geonameIds as f1, [place].[Geonames] as f2
+		Where f1.GeonameId=f2.GeonameId and f2.Admin1GeonameId IS NOT NULL
+		Union
+		Select Distinct f2.[CountryGeonameId]
+		From @tbl_geonameIds as f1, [place].[Geonames] as f2
+		Where f1.GeonameId=f2.GeonameId and f2.[CountryGeonameId] IS NOT NULL
+		Except
+		Select GeonameId
+		From [place].[ActiveGeonames]
 	--Insert new
 	Insert into [place].[ActiveGeonames] ([GeonameId]
 		  ,[Name]
