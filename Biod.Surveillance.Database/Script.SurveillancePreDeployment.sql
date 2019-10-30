77e6dbb752ed90e90a1919e6966f2407342b6aed
Vivian Hu <vivian.hu@bluedot.global>
2019-10-30 15:00:23 +0000

    Merged in feature/PT-307-adding-new-surveillance-article-f (pull request #67)
    Feature/PT-307 adding new surveillance article f
    
    * usp_ZebraEventGetArticlesByEventId
    
    * usp_ZebraEventGetArticlesByEventId
    
    * Populate [surveillance].[ArticleFeed] with RSS
    
    * Populate [surveillance].[ArticleFeed] with RSS in surveillance
    
    Approved-by: Basam Ahmad

--- Biod.Surveillance.Database/Script.SurveillancePreDeployment.sql
+++ Biod.Surveillance.Database/Script.SurveillancePreDeployment.sql
@@ -9,30 +9,35 @@
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
-print 'rename a column' 
-IF NOT EXISTS(SELECT 1 FROM sys.columns 
-				WHERE Name = N'DiseaseType' AND Object_ID = Object_ID(N'disease.Diseases'))
-	EXEC sp_rename 'disease.Diseases.Type', 'DiseaseType', 'COLUMN'; 
-GO
+--pt-307 vivian
+IF NOT EXISTS(SELECT 1 FROM [surveillance].[ArticleFeed] WHERE [ArticleFeedId] = 9)
+	Insert into [surveillance].[ArticleFeed]([ArticleFeedId], [ArticleFeedName])
+	Values(9, 'RSS')
 
-print 'preserve disease info'
-If EXISTS(SELECT 1 FROM sys.columns 
-				WHERE Name = N'IncubationAverageDays' AND Object_ID = Object_ID(N'disease.Diseases'))
-Begin
-	Drop Table If Exists [disease].tmp_disease
+--print 'rename a column' 
+--IF NOT EXISTS(SELECT 1 FROM sys.columns 
+--				WHERE Name = N'DiseaseType' AND Object_ID = Object_ID(N'disease.Diseases'))
+--	EXEC sp_rename 'disease.Diseases.Type', 'DiseaseType', 'COLUMN'; 
+--GO
 
-	Declare @sqlStr varchar(8000) ='
-	Select DiseaseId, [IncubationAverageDays], [IncubationMinimumDays], [IncubationMaximumDays],
-		[SymptomaticAverageDays], [SymptomaticMinimumDays], [SymptomaticMaximumDays]
-	Into [disease].tmp_disease From [disease].[Diseases]'
-	EXEC (@sqlStr)
+--print 'preserve disease info'
+--If EXISTS(SELECT 1 FROM sys.columns 
+--				WHERE Name = N'IncubationAverageDays' AND Object_ID = Object_ID(N'disease.Diseases'))
+--Begin
+--	Drop Table If Exists [disease].tmp_disease
 
-	--may need
-	Alter Table [disease].[Diseases] Drop Column If Exists [IncubationAverageDays], 
-		Column If Exists [IncubationMinimumDays], 
-		Column If Exists [IncubationMaximumDays],
-		Column If Exists [SymptomaticAverageDays], 
-		Column If Exists [SymptomaticMinimumDays], 
-		Column If Exists [SymptomaticMaximumDays]
+--	Declare @sqlStr varchar(8000) ='
+--	Select DiseaseId, [IncubationAverageDays], [IncubationMinimumDays], [IncubationMaximumDays],
+--		[SymptomaticAverageDays], [SymptomaticMinimumDays], [SymptomaticMaximumDays]
+--	Into [disease].tmp_disease From [disease].[Diseases]'
+--	EXEC (@sqlStr)
+
+--	--may need
+--	Alter Table [disease].[Diseases] Drop Column If Exists [IncubationAverageDays], 
+--		Column If Exists [IncubationMinimumDays], 
+--		Column If Exists [IncubationMaximumDays],
+--		Column If Exists [SymptomaticAverageDays], 
+--		Column If Exists [SymptomaticMinimumDays], 
+--		Column If Exists [SymptomaticMaximumDays]
 		
-End
+--End
