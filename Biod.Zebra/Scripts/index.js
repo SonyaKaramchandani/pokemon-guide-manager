fb1b6706ed211855cdecb22c5f144209871a1244
Kevin Chu <kevin.chu@bluedot.global>
2019-11-14 18:10:51 +0000

    Add saving of sort/group by option to cookie on client.
    Added a utils for interacting with browser cookies (sourced from
    Mozilla developer reference). Added saving of the user selected option
    for sort and group by drop down list.
    
    Ref: https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie/Simple_document.cookie_framework
    Part of PT-455.
    Merged in feature/PT-455-save-cookie-on-client (pull request #124)
    Approved-by: Gursharn Singh <gursharn.singh@bluedot.global>

--- Biod.Zebra/Scripts/index.js
+++ Biod.Zebra/Scripts/index.js
@@ -1,7 +1,11 @@
 ﻿import esri3Map from '../src/app/map/index';
+import docCookies from '../src/app/utils/cookies';
 
 window.biod = window.biod || {};
 window.biod.urls = window.biod.urls || {};
 window.biod.map = window.biod.map || {};
+window.biod.utils = window.biod.utils || {};
 
 window.biod.map.renderMap = esri3Map.renderMap;
+
+window.biod.utils.docCookies = docCookies;
