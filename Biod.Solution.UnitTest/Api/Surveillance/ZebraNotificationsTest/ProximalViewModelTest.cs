dd7826c6452c4732a3a162736fbf64208ba4083a
Anna Caldwell <anna.caldwell@bluedot.global>
2019-11-27 12:54:26 +0000

    ProximalViewModel has been updated to use EventCaseCountModel to populate e-mail data. proximal email template has also been updated to send slightly modified versions based on the event location type.
    EF has also been updated to add  `usp_ZebraEventGetProximalUsersByEventId`. This SP is a simplified version of  `usp_ZebraEmailGetProximalEmailData` that ProximalViewModel used to reference. Since nesting has been moved into C#, this SP is much simpler.
    
    Microsoft.SqlServer.Types package has also been added. This is needed when ActiveGeonames was included in a query since it has spatial data.
    
    Some bugs in EventCaseCountModel have also been fixed.
    
    Part of PT-357, PT-478, PT-479, PT-504, PT-558
    Merged in feature/PT-288-proximal-recipient (pull request #153)
    Approved-by: Kevin Chu
    
    (cherry picked from commit 195ebc054e26b69eb6d9cf517067e0bed96f48d8)

--- Biod.Solution.UnitTest/Api/Surveillance/ZebraNotificationsTest/ProximalViewModelTest.cs
+++ Biod.Solution.UnitTest/Api/Surveillance/ZebraNotificationsTest/ProximalViewModelTest.cs
@@ -7,6 +7,7 @@ using Moq;
 using System.Collections.Generic;
 using System.Data.Entity.Core.Objects;
 using System.Linq;
+using Constants = Biod.Zebra.Library.Infrastructures.Constants;
 
 namespace Biod.Solution.UnitTest.Api.Surveillance.ZebraNotificationsTest
 {
@@ -16,10 +17,13 @@ namespace Biod.Solution.UnitTest.Api.Surveillance.ZebraNotificationsTest
     [TestClass()]
     public class ProximalViewModelTest : NotificationTest
     {
+        ProximalMockDbSet mockDbSet;
+
         [TestInitialize()]
         public override void Initialize()
         {
             base.Initialize();
+            mockDbSet = new ProximalMockDbSet(mockDbContext);
 
             var mockUserStore = new Mock<IUserStore<ApplicationUser>>();
             mockUserManager = new Mock<UserManager<ApplicationUser>>(mockUserStore.Object);
@@ -27,100 +31,212 @@ namespace Biod.Solution.UnitTest.Api.Surveillance.ZebraNotificationsTest
         }
 
         [TestMethod()]
-        public void EmptyProximalEvents()
+        public void NoProximalUsers()
         {
             var eventItem = new Event
             {
                 DiseaseId = random.Next(100),
                 EventId = random.Next(1000, 9999),
-                EventTitle = "Test Event to User with Unconfirmed E-mail"
+                EventTitle = "Test Event with No Proximal Users"
             };
             mockDbContext.Setup(context => context.Events).ReturnsDbSet(new List<Event> { eventItem });
-            var mockProximalEmailObject = new Mock<ObjectResult<usp_ZebraEmailGetProximalEmailData_Result>>();
-            mockProximalEmailObject.Setup(x => x.GetEnumerator()).Returns(new List<usp_ZebraEmailGetProximalEmailData_Result>().GetEnumerator());
-            mockDbContext.Setup(context => context.usp_ZebraEmailGetProximalEmailData(eventItem.EventId)).Returns(mockProximalEmailObject.Object);
+            var mockProximalUsers = new Mock<ObjectResult<usp_ZebraEventGetProximalUsersByEventId_Result>>();
+            mockProximalUsers.Setup(x => x.GetEnumerator()).Returns(new List<usp_ZebraEventGetProximalUsersByEventId_Result>().GetEnumerator());
+            mockDbContext.Setup(context => context.usp_ZebraEventGetProximalUsersByEventId(eventItem.EventId)).Returns(mockProximalUsers.Object);
 
             Assert.AreEqual(0, ProximalViewModel.GetNotificationViewModelList(mockDbContext.Object, mockUserManager.Object, eventItem.EventId).Count());
         }
 
         [TestMethod()]
-        public void EmailNotConfirmed()
+        public void EventHasNoPositiveDelta()
         {
             var user = CreateMockUser();
-            user.EmailConfirmed = false;
+            var geonameId = random.Next();
+            user.AoiGeonameIds = $"{geonameId}";
             mockUserManager.Setup(mgr => mgr.Users).Returns(new List<ApplicationUser> { user }.AsQueryable());
 
             var eventItem = new Event
             {
                 DiseaseId = random.Next(100),
                 EventId = random.Next(1000, 9999),
-                EventTitle = "Test Event to User with Unconfirmed E-mail"
+                EventTitle = "Test Event with No Positive Delta"
             };
             mockDbContext.Setup(context => context.Events).ReturnsDbSet(new List<Event> { eventItem });
-            var mockProximalEmail = new List<usp_ZebraEmailGetProximalEmailData_Result> { CreateMockZebraEmailGetProximalEmailDataResult(user) };
-            var mockProximalEmailObject = new Mock<ObjectResult<usp_ZebraEmailGetProximalEmailData_Result>>();
-            mockProximalEmailObject.Setup(x => x.GetEnumerator()).Returns(mockProximalEmail.GetEnumerator());
-            mockDbContext.Setup(context => context.usp_ZebraEmailGetProximalEmailData(eventItem.EventId)).Returns(mockProximalEmailObject.Object);
+            var mockProximalUsers = new Mock<ObjectResult<usp_ZebraEventGetProximalUsersByEventId_Result>>();
+
+            mockProximalUsers.Setup(x => x.GetEnumerator()).Returns(new List<usp_ZebraEventGetProximalUsersByEventId_Result>
+            {
+                new usp_ZebraEventGetProximalUsersByEventId_Result { UserId = user.Id, GeonameId = geonameId }
+            }.GetEnumerator());
+            mockDbContext.Setup(context => context.usp_ZebraEventGetProximalUsersByEventId(eventItem.EventId)).Returns(mockProximalUsers.Object);
+            mockDbSet.AddEventToEventLocationTables(eventItem.EventId, geonameId, Constants.LocationType.CITY, false);
 
             Assert.AreEqual(0, ProximalViewModel.GetNotificationViewModelList(mockDbContext.Object, mockUserManager.Object, eventItem.EventId).Count());
         }
 
         [TestMethod()]
-        public void NewCaseNotificationDisabled()
+        public void EventHasPositiveDelta()
         {
             var user = CreateMockUser();
-            user.NewCaseNotificationEnabled = false;
+            var geonameId = random.Next();
+            user.AoiGeonameIds = $"{geonameId}";
             mockUserManager.Setup(mgr => mgr.Users).Returns(new List<ApplicationUser> { user }.AsQueryable());
 
             var eventItem = new Event
             {
                 DiseaseId = random.Next(100),
                 EventId = random.Next(1000, 9999),
-                EventTitle = "Test Event to User with Notification Disabled"
+                EventTitle = "Proximal Test Event with Positive Delta"
             };
             mockDbContext.Setup(context => context.Events).ReturnsDbSet(new List<Event> { eventItem });
-            var mockProximalEmail = new List<usp_ZebraEmailGetProximalEmailData_Result> { CreateMockZebraEmailGetProximalEmailDataResult(user) };
-            var mockProximalEmailObject = new Mock<ObjectResult<usp_ZebraEmailGetProximalEmailData_Result>>();
-            mockProximalEmailObject.Setup(x => x.GetEnumerator()).Returns(mockProximalEmail.GetEnumerator());
-            mockDbContext.Setup(context => context.usp_ZebraEmailGetProximalEmailData(eventItem.EventId)).Returns(mockProximalEmailObject.Object);
+            var mockProximalUsers = new Mock<ObjectResult<usp_ZebraEventGetProximalUsersByEventId_Result>>();
 
-            Assert.AreEqual(0, ProximalViewModel.GetNotificationViewModelList(mockDbContext.Object, mockUserManager.Object, eventItem.EventId).Count());
-        }
-
-        [TestMethod()]
-        public void ValidProximalEvents()
-        {
-            var user = CreateMockUser();
-            mockUserManager.Setup(mgr => mgr.Users).Returns(new List<ApplicationUser> { user }.AsQueryable());
-
-            var eventItem = new Event
+            mockProximalUsers.Setup(x => x.GetEnumerator()).Returns(new List<usp_ZebraEventGetProximalUsersByEventId_Result>
             {
-                DiseaseId = random.Next(100),
-                EventId = random.Next(1000, 9999),
-                EventTitle = "Proximal Test Event"
-            };
-            mockDbContext.Setup(context => context.Events).ReturnsDbSet(new List<Event> { eventItem });
-            var mockProximalEmail = new List<usp_ZebraEmailGetProximalEmailData_Result> { CreateMockZebraEmailGetProximalEmailDataResult(user) };
-            var mockProximalEmailObject = new Mock<ObjectResult<usp_ZebraEmailGetProximalEmailData_Result>>();
-            mockProximalEmailObject.Setup(x => x.GetEnumerator()).Returns(mockProximalEmail.GetEnumerator());
-            mockDbContext.Setup(context => context.usp_ZebraEmailGetProximalEmailData(eventItem.EventId)).Returns(mockProximalEmailObject.Object);
+                new usp_ZebraEventGetProximalUsersByEventId_Result { UserId = user.Id, GeonameId = geonameId }
+            }.GetEnumerator());
+            mockDbContext.Setup(context => context.usp_ZebraEventGetProximalUsersByEventId(eventItem.EventId)).Returns(mockProximalUsers.Object);
+            mockDbSet.AddEventToEventLocationTables(eventItem.EventId, geonameId, Constants.LocationType.CITY, true);
+
             var mockGeonameIdObject = new Mock<ObjectResult<usp_SearchGeonamesByGeonameIds_Result>>();
-            mockGeonameIdObject.Setup(x => x.GetEnumerator()).Returns(new List<usp_SearchGeonamesByGeonameIds_Result>().GetEnumerator());
+            mockGeonameIdObject.Setup(x => x.GetEnumerator()).Returns(new List<usp_SearchGeonamesByGeonameIds_Result>
+            {
+                new usp_SearchGeonamesByGeonameIds_Result { GeonameId = geonameId, DisplayName = "", LocationType = Constants.LocationTypeDescription.CITY }
+            }.GetEnumerator());
             mockDbContext.Setup(context => context.usp_SearchGeonamesByGeonameIds(user.AoiGeonameIds)).Returns(mockGeonameIdObject.Object);
             var mockDiseaseNameObject = new Mock<ObjectResult<string>>();
-            mockDiseaseNameObject.Setup(x => x.GetEnumerator()).Returns(new List<string> { "" }.GetEnumerator());
+            mockDiseaseNameObject.Setup(x => x.GetEnumerator()).Returns(new List<string> { "Disease Name" }.GetEnumerator());
             mockDbContext.Setup(context => context.usp_ZebraEventGetDiseaseNameByEventId(eventItem.EventId)).Returns(mockDiseaseNameObject.Object);
 
-            Assert.AreEqual(1, ProximalViewModel.GetNotificationViewModelList(mockDbContext.Object, mockUserManager.Object, eventItem.EventId).Count());
+            var notifications = ProximalViewModel.GetNotificationViewModelList(mockDbContext.Object, mockUserManager.Object, eventItem.EventId);
+            Assert.AreEqual(1, notifications.Count());
+            Assert.AreEqual(geonameId, ((ProximalViewModel)notifications[0]).EventGeonameId);
+            Assert.AreEqual(user.Id, ((ProximalViewModel)notifications[0]).UserId);
         }
 
-        private usp_ZebraEmailGetProximalEmailData_Result CreateMockZebraEmailGetProximalEmailDataResult(ApplicationUser user)
+        [TestMethod()]
+        public void UserHasMultipleAoi()
         {
-            return new usp_ZebraEmailGetProximalEmailData_Result
+            var user = CreateMockUser();
+            var geonameId1 = random.Next();
+            var geonameId2 = random.Next();
+            var geonameId3 = random.Next();
+            user.AoiGeonameIds = $"{geonameId1},{geonameId2},{geonameId3}";
+            mockUserManager.Setup(mgr => mgr.Users).Returns(new List<ApplicationUser> { user }.AsQueryable());
+
+            var eventItem = new Event
             {
-                UserId = user.Id,
-                Email = user.Email
+                DiseaseId = random.Next(100),
+                EventId = random.Next(1000, 9999),
+                EventTitle = "Proximal Test Event Affecting Multiple Locations"
             };
+            mockDbContext.Setup(context => context.Events).ReturnsDbSet(new List<Event> { eventItem });
+
+            var mockProximalUsers = new List<usp_ZebraEventGetProximalUsersByEventId_Result>
+            {
+                new usp_ZebraEventGetProximalUsersByEventId_Result { UserId = user.Id, GeonameId = geonameId1 },
+                new usp_ZebraEventGetProximalUsersByEventId_Result { UserId = user.Id, GeonameId = geonameId2 }
+            };
+            var mockProximalUsersObject = new Mock<ObjectResult<usp_ZebraEventGetProximalUsersByEventId_Result>>();
+            mockProximalUsersObject.Setup(x => x.GetEnumerator()).Returns(mockProximalUsers.GetEnumerator());
+            mockDbContext.Setup(context => context.usp_ZebraEventGetProximalUsersByEventId(eventItem.EventId)).Returns(mockProximalUsersObject.Object);
+            mockDbSet.AddEventToEventLocationTables(eventItem.EventId, geonameId1, Constants.LocationType.CITY, true);
+            mockDbSet.AddEventToEventLocationTables(eventItem.EventId, geonameId2, Constants.LocationType.PROVINCE, true);
+
+            var mockGeonameIdObject = new Mock<ObjectResult<usp_SearchGeonamesByGeonameIds_Result>>();
+            mockGeonameIdObject.Setup(x => x.GetEnumerator()).Returns(new List<usp_SearchGeonamesByGeonameIds_Result>
+            {
+                new usp_SearchGeonamesByGeonameIds_Result { GeonameId = geonameId1, DisplayName = "", LocationType = Constants.LocationTypeDescription.CITY },
+                new usp_SearchGeonamesByGeonameIds_Result { GeonameId = geonameId2, DisplayName = "", LocationType = Constants.LocationTypeDescription.PROVINCE },
+                new usp_SearchGeonamesByGeonameIds_Result { GeonameId = geonameId3, DisplayName = "", LocationType = Constants.LocationTypeDescription.CITY }
+            }.GetEnumerator());
+            mockDbContext.Setup(context => context.usp_SearchGeonamesByGeonameIds(user.AoiGeonameIds)).Returns(mockGeonameIdObject.Object);
+            var mockDiseaseNameObject = new Mock<ObjectResult<string>>();
+            mockDiseaseNameObject.Setup(x => x.GetEnumerator()).Returns(new List<string> { "Disease Name" }.GetEnumerator());
+            mockDbContext.Setup(context => context.usp_ZebraEventGetDiseaseNameByEventId(eventItem.EventId)).Returns(mockDiseaseNameObject.Object);
+
+            var notifications = ProximalViewModel.GetNotificationViewModelList(mockDbContext.Object, mockUserManager.Object, eventItem.EventId);
+            Assert.AreEqual(2, notifications.Count());
+            ValidateProximalNotificationList(notifications, mockProximalUsers);
+        }
+
+        [TestMethod()]
+        public void MultipleUsersWithDifferentAoi()
+        {
+            var user1 = CreateMockUser();
+            var user2 = CreateMockUser();
+            var geonameId1 = random.Next();
+            var geonameId2 = random.Next();
+            var geonameId3 = random.Next();
+            user1.AoiGeonameIds = $"{geonameId1},{geonameId2}";
+            user2.AoiGeonameIds = $"{geonameId2},{geonameId3}";
+            mockUserManager.Setup(mgr => mgr.Users).Returns(new List<ApplicationUser> { user1, user2 }.AsQueryable());
+
+            var eventItem = new Event
+            {
+                DiseaseId = random.Next(100),
+                EventId = random.Next(1000, 9999),
+                EventTitle = "Proximal Test Event Affecting Multiple Locations"
+            };
+            mockDbContext.Setup(context => context.Events).ReturnsDbSet(new List<Event> { eventItem });
+
+            var mockProximalUsers = new List<usp_ZebraEventGetProximalUsersByEventId_Result>
+            {
+                new usp_ZebraEventGetProximalUsersByEventId_Result { UserId = user1.Id, GeonameId = geonameId1 },
+                new usp_ZebraEventGetProximalUsersByEventId_Result { UserId = user1.Id, GeonameId = geonameId2 },
+                new usp_ZebraEventGetProximalUsersByEventId_Result { UserId = user2.Id, GeonameId = geonameId2 }
+            };
+            var mockProximalUsersObject = new Mock<ObjectResult<usp_ZebraEventGetProximalUsersByEventId_Result>>();
+            mockProximalUsersObject.Setup(x => x.GetEnumerator()).Returns(mockProximalUsers.GetEnumerator());
+            mockDbContext.Setup(context => context.usp_ZebraEventGetProximalUsersByEventId(eventItem.EventId)).Returns(mockProximalUsersObject.Object);
+            mockDbSet.AddEventToEventLocationTables(eventItem.EventId, geonameId1, Constants.LocationType.CITY, true);
+            mockDbSet.AddEventToEventLocationTables(eventItem.EventId, geonameId2, Constants.LocationType.PROVINCE, true);
+
+            var mockUser1GeonameIdObject = new Mock<ObjectResult<usp_SearchGeonamesByGeonameIds_Result>>();
+            mockUser1GeonameIdObject.Setup(x => x.GetEnumerator()).Returns(new List<usp_SearchGeonamesByGeonameIds_Result>
+            {
+                new usp_SearchGeonamesByGeonameIds_Result { GeonameId = geonameId1, DisplayName = "", LocationType = Constants.LocationTypeDescription.CITY },
+                new usp_SearchGeonamesByGeonameIds_Result { GeonameId = geonameId2, DisplayName = "", LocationType = Constants.LocationTypeDescription.PROVINCE }
+            }.GetEnumerator());
+            var mockUser2GeonameIdObject = new Mock<ObjectResult<usp_SearchGeonamesByGeonameIds_Result>>();
+            mockUser2GeonameIdObject.Setup(x => x.GetEnumerator()).Returns(new List<usp_SearchGeonamesByGeonameIds_Result>
+            {
+                new usp_SearchGeonamesByGeonameIds_Result { GeonameId = geonameId2, DisplayName = "", LocationType = Constants.LocationTypeDescription.PROVINCE },
+                new usp_SearchGeonamesByGeonameIds_Result { GeonameId = geonameId3, DisplayName = "", LocationType = Constants.LocationTypeDescription.CITY }
+            }.GetEnumerator());
+            mockDbContext.Setup(context => context.usp_SearchGeonamesByGeonameIds(user1.AoiGeonameIds)).Returns(mockUser1GeonameIdObject.Object);
+            mockDbContext.Setup(context => context.usp_SearchGeonamesByGeonameIds(user2.AoiGeonameIds)).Returns(mockUser2GeonameIdObject.Object);
+            var mockDiseaseNameObject = new Mock<ObjectResult<string>>();
+            mockDiseaseNameObject.Setup(x => x.GetEnumerator()).Returns(new List<string> { "Disease Name" }.GetEnumerator());
+            mockDbContext.Setup(context => context.usp_ZebraEventGetDiseaseNameByEventId(eventItem.EventId)).Returns(mockDiseaseNameObject.Object);
+
+            var notifications = ProximalViewModel.GetNotificationViewModelList(mockDbContext.Object, mockUserManager.Object, eventItem.EventId);
+            Assert.AreEqual(3, notifications.Count());
+            ValidateProximalNotificationList(notifications, mockProximalUsers);
+        }
+
+        private void ValidateProximalNotificationList(List<NotificationViewModel> notifications, List<usp_ZebraEventGetProximalUsersByEventId_Result> proximalUsers)
+        {
+            var usersById = proximalUsers.GroupBy(u => u.UserId)
+                .ToDictionary(g => g.Key, g => new HashSet<int>(g.Where(r => r.GeonameId != null).Select(r => (int)r.GeonameId)));
+            Assert.IsTrue(notifications
+                .GroupBy(n => n.UserId)
+                .All(g =>
+                {
+                    if (!usersById.ContainsKey(g.Key) || g.Count() != usersById[g.Key].Count())
+                    {
+                        return false;
+                    }
+                    foreach (ProximalViewModel model in g)
+                    {
+                        if (!usersById[model.UserId].Contains(model.EventGeonameId))
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }));
         }
     }
 }
