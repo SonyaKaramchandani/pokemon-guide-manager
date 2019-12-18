a9d23e6f90a5af858b1fc2263071a4bfb0bc22b1
Anna Caldwell <anna.caldwell@bluedot.global>
2019-12-18 03:39:11 +0000

    Add utility function for getting icon based on location type
    These icons are being used in multiple places (legend, outbreak extent, user AOI) so it makes sense to have a common place for generating them
    
    Base64 encoding of assets has also been added. Esri requires encoded strings for assets so this utility function would be useful.
    
    Part of PT-676.
    Merged in feature/pt-676-update-outbreak-pins (pull request #213)
    Approved-by: Sonya Karamchandani

---
+++ Biod.Zebra/src/app/map/constants.js
@@ -0,0 +1,8 @@
+﻿export const ID_AIRPORT_ICON_LAYER = 'biod.map.airport.layer.icon';
+export const ID_AIRPORT_RISK_LAYER = 'biod.map.airport.layer.risk';
+
+export const locationTypes = {
+  CITY: 'city',
+  PROVINCE: 'province',
+  COUNTRY: 'country'
+};
