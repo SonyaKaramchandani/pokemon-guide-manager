f1836e741fb05ab7697c411eeb7a39e91587c758
Vivian Hu <vivian.hu@bluedot.global>
2019-12-19 15:58:52 +0000

    Merged in feature/PT-711-adds-db-tables (pull request #217)
    New tables for new model
    
    * New tables for new model
    
    * Changed the surfix to SpreadMd
    
    Approved-by: Basam Ahmad

---
+++ Biod.Zebra.Database/zebra/Tables/EventSourceAirportSpreadMd.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [zebra].[EventSourceAirportSpreadMd] (
+    [EventId]         INT            NOT NULL,
+    [SourceStationId] INT            NOT NULL,
+    [StationName]     NVARCHAR (64)  NULL,
+    [StationCode]     CHAR (3)       NULL,
+    [CityDisplayName] NVARCHAR (200) NULL,
+    [CountryName]     NVARCHAR (100) NULL,
+    [NumCtryAirports] INT            NULL,
+    [Volume]          INT            NULL,
+    [CtryRank]        INT            NULL,
+    [WorldRank]       INT            NULL,
+	MinPrevelance	  FLOAT			 NOT NULL,
+	MaxPrevelance	  FLOAT			 NOT NULL
+    CONSTRAINT [PK_EventSourceAirportSpreadMd] PRIMARY KEY CLUSTERED ([EventId] ASC, [SourceStationId] ASC),
+    CONSTRAINT [FK_EventSourceAirportSpreadMd_EventId] FOREIGN KEY ([EventId]) REFERENCES [surveillance].[Event] ([EventId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_EventSourceAirportSpreadMd_StationId] FOREIGN KEY ([SourceStationId]) REFERENCES [zebra].[Stations] ([StationId]) ON DELETE CASCADE
+);
+
