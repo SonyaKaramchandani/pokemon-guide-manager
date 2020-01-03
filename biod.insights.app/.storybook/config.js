ea2e2231f1a4b3ef89d506a345fe72ac935a4b0d
Gursharn Singh <gursharn.singh@bluedot.global>
2020-01-03 15:03:54 +0000

    Semantic ui theming and LESS support for storybook

--- biod.insights.app/.storybook/config.js
+++ biod.insights.app/.storybook/config.js
@@ -1,6 +1,6 @@
 import { configure, addDecorator } from '@storybook/react';
 import ThemeDecorator from './themeDecorator';
-import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-less/semantic.less';
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/components', true, /\.stories\.js$/), module);
