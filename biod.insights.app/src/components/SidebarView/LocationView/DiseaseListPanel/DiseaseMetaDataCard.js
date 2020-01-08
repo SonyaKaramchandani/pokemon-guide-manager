c631b34968a98cc0229c7d5203e98645d98153d4
bluedot-mtemkine <mikhail.temkine@bluedot.global>
2020-01-08 11:20:03 -0500

    MT - apply FlexGroup and Typography to DiseaseMetaDataCard

--- biod.insights.app/src/components/SidebarView/LocationView/DiseaseListPanel/DiseaseMetaDataCard.js
+++ biod.insights.app/src/components/SidebarView/LocationView/DiseaseListPanel/DiseaseMetaDataCard.js
@@ -4,7 +4,10 @@ import React from 'react';
 import { Header, Icon, Image, Grid } from 'semantic-ui-react';
 import ImportationSvg from 'assets/importation.svg';
 import ExportationSvg from 'assets/exportation.svg';
+import MapMarkerSvg from 'assets/map-marker.svg';
 import { getTravellerInterval } from 'utils/stringFormatingHelpers';
+import { Typography } from 'components/_common/Typography';
+import { FlexGroup } from 'components/_common/FlexGroup';
 
 const formatReportedCases = reportedCases => {
   if (reportedCases === null) {
@@ -26,18 +29,23 @@ const DiseaseMetaDataCard = ({ casesInfo, importationRisk, exportationRisk }) =>
     <Grid columns={2} divided>
       <Grid.Row>
         <Grid.Column>
-          <div sx={{ color: 'gray1' }}>Number of cases reported in or near your location</div>
+          <div sx={{ mb: '9px' }}>
+            <Typography variant="caption" color="gray1">Number of cases reported in or near your location</Typography>
+          </div>
           <div sx={{ display: 'flex', alignItems: 'start' }}>
-            <Icon name="map marker alternate" sx={{ minWidth: '1.18em' }} />
-            <Header size="small">{formattedReportedCases}</Header>
+            <FlexGroup prefixImg={MapMarkerSvg}>
+              <Typography variant="subtitle1">{formattedReportedCases}</Typography>
+            </FlexGroup>
           </div>
         </Grid.Column>
         <Grid.Column>
-          <div sx={{ color: 'gray1' }}>Projected number of infected travellers/month</div>
+          <div sx={{ mb: '9px' }}>
+            <Typography variant="caption" color="gray1">Projected number of infected travellers/month</Typography>
+          </div>
           <div sx={{ display: 'flex', alignItems: 'start' }}>
-            {importationRisk && <Image spaced="right" src={ImportationSvg} />}
-            {!importationRisk && <Image spaced="right" src={ExportationSvg} />}
-            <Header size="small">{travellers}</Header>
+            <FlexGroup prefixImg={importationRisk ? ImportationSvg : ExportationSvg}>
+              <Typography variant="subtitle1">{travellers}</Typography>
+            </FlexGroup>
           </div>
         </Grid.Column>
       </Grid.Row>
