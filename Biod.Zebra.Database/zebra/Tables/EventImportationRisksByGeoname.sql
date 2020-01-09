9f8f5ecb537ec00591e3010954e6680e1ac4c9ca
Vivian Hu <vivian.hu@bluedot.global>
2020-01-09 21:08:35 +0000

    Merged in feature/PT-742-752-SPs (pull request #250)
    Feature/PT-742 752 SPs
    
    * usp_ZebraDataRenderSetImportationRiskByGeonameId
    
    * usp_ZebraDataRenderSetImportationRiskByGeonameId to include all aois
    
    * foreign key issue
    
    * usp_ZebraDataRenderSetGeonameImportationRiskByEventId
    
    * EventImportationRisksByGeoname
    
    Approved-by: Predrag Ostojic

--- Biod.Zebra.Database/zebra/Tables/EventImportationRisksByGeoname.sql
+++ Biod.Zebra.Database/zebra/Tables/EventImportationRisksByGeoname.sql
@@ -1,17 +1,17 @@
 ﻿CREATE TABLE zebra.EventImportationRisksByGeoname (
+	EventId int NOT NULL,
 	GeonameId int NOT NULL,
 	LocalSpread int NOT NULL,
-	EventId int NOT NULL,
 	MinProb decimal(5,4),
 	MaxProb decimal(5,4),
 	MinVolume decimal(10,3),
 	MaxVolume decimal(10,3)
-CONSTRAINT PK_EventImportationRisksByGeoname PRIMARY KEY CLUSTERED (GeonameId, EventId),
+CONSTRAINT PK_EventImportationRisksByGeoname PRIMARY KEY CLUSTERED (EventId, GeonameId),
 CONSTRAINT [FK_EventImportationRisksByGeonamer_GeonameId] FOREIGN KEY (GeonameId) REFERENCES place.Geonames(GeonameId) ON DELETE cascade,
 CONSTRAINT [FK_EventImportationRisksByGeoname_EventId] FOREIGN KEY (EventId) REFERENCES [surveillance].[Event](EventId) ON DELETE cascade
 );
 
 GO
 
-CREATE INDEX idx_EventImportationRisksByGeoname ON zebra.EventImportationRisksByGeoname(EventId ASC);
+CREATE INDEX idx_EventImportationRisksByGeoname_GeonameId ON zebra.EventImportationRisksByGeoname(GeonameId ASC);
 GO
