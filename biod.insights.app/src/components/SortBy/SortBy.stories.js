7a79dc7da3f9e2e0b64c412188fa6b516decfc71
bluedot-mtemkine <mikhail.temkine@bluedot.global>
2020-01-14 18:12:51 -0500

    MT - add to SortBy.stories

--- biod.insights.app/src/components/SortBy/SortBy.stories.js
+++ biod.insights.app/src/components/SortBy/SortBy.stories.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import SortBy from './SortBy';
+import { LocationListSortOptions } from 'components/SidebarView/SortByOptions';
 
 export default {
   title: 'Controls/SortBy'
 };
 
-export const text = () => <SortBy>Hello SortBy</SortBy>;
+export const text = () => <SortBy
+  selectedValue={LocationListSortOptions[0].value}
+  options={LocationListSortOptions}
+  onSelect={action('onSelect')}
+/>;
