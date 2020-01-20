170466f1878d855647e87322eed8a021c2167e63
bluedot-mtemkine <mikhail.temkine@bluedot.global>
2020-01-20 14:19:40 -0500

    MT - add CBcode debug component

--- biod.insights.app/src/components/_common/SemanticUiTests/SemanticUi.stories.js
+++ biod.insights.app/src/components/_common/SemanticUiTests/SemanticUi.stories.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { jsx } from 'theme-ui';
-import { Tab } from 'semantic-ui-react';
+import { Tab, Grid } from 'semantic-ui-react';
 import { Typography } from 'components/_common/Typography';
 
 export default {
@@ -24,6 +24,47 @@ export const tabs1 = () => (
         { menuItem: 'Events', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
         { menuItem: '3rd tab', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
       ]} onTabChange={action('handleOnTabChange')} />
+    </div>
+  </>
+);
+
+export const gridDivided = () => (
+  <>
+    <div style={{ width: 370, padding: '10px' }}>
+      <Grid columns={2} divided='vertically'>
+        <Grid.Row divided>
+          <Grid.Column>
+            <Typography variant="caption" color="deepSea50">cell content</Typography>
+            <Typography variant="subtitle2" color="stone90">value</Typography>
+          </Grid.Column>
+          <Grid.Column>
+            <Typography variant="caption" color="deepSea50">cell content</Typography>
+            <Typography variant="subtitle2" color="stone90">value</Typography>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row divided>
+          <Grid.Column>
+            <Typography variant="caption" color="deepSea50">cell content</Typography>
+            <Typography variant="subtitle2" color="stone90">value</Typography>
+          </Grid.Column>
+          <Grid.Column>
+            <Typography variant="caption" color="deepSea50">cell content</Typography>
+            <Typography variant="subtitle2" color="stone90">value</Typography>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row divided>
+          <Grid.Column>
+            <Typography variant="caption" color="deepSea50">cell content</Typography>
+            <Typography variant="subtitle2" color="stone90">value</Typography>
+          </Grid.Column>
+          <Grid.Column>
+            <Typography variant="caption" color="deepSea50">cell content</Typography>
+            <Typography variant="subtitle2" color="stone90">value</Typography>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   </>
 );
