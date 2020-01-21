4188ceb2bbcc0dee46e346666a9b1a6c5dc2dad9
Gursharn Singh <gursharn.singh@bluedot.global>
2020-01-21 19:40:51 +0000

    Show dash in-case model is not run for a risk
    Merged in bugfix/PT-824-insights---locstrat---likelihood- (pull request #317)
    
    Approved-by: Kevin Chu

--- biod.insights.app/src/components/SidebarView/LocationView/DiseaseListPanel/DiseaseMetaDataCard.js
+++ biod.insights.app/src/components/SidebarView/LocationView/DiseaseListPanel/DiseaseMetaDataCard.js
@@ -7,43 +7,63 @@ import { Typography } from 'components/_common/Typography';
 import { FlexGroup } from 'components/_common/FlexGroup';
 import { BdIcon } from 'components/_common/BdIcon';
 
-
 const DiseaseMetaDataCard = ({ caseCounts, importationRisk, exportationRisk }) => {
   const { reportedCases } = caseCounts;
-  const formattedReportedCases = (reportedCases > 0)
-    ? formatNumber(reportedCases, 'case')
-    : `No cases reported in or near your location`;
+  const formattedReportedCases =
+    reportedCases > 0
+      ? formatNumber(reportedCases, 'case')
+      : `No cases reported in or near your location`;
 
   const risk = importationRisk || exportationRisk;
-  const travellers = risk ? getTravellerInterval(risk.minMagnitude, risk.maxMagnitude, true) : '-';
+  const travellers = risk
+    ? getTravellerInterval(risk.minMagnitude, risk.maxMagnitude, true, risk.isModelNotRun)
+    : '-';
 
   return (
-    <Grid columns={2} divided sx={{
-      '.icon.bd-icon': {
-        fontSize: '14px'
-      }
-    }}>
+    <Grid
+      columns={2}
+      divided
+      sx={{
+        '.icon.bd-icon': {
+          fontSize: '14px'
+        }
+      }}
+    >
       <Grid.Row>
         <Grid.Column>
           <div sx={{ mb: '9px' }}>
-            <Typography variant="caption" color="deepSea50">Number of cases reported in or near your location</Typography>
+            <Typography variant="caption" color="deepSea50">
+              Number of cases reported in or near your location
+            </Typography>
           </div>
           <div sx={{ display: 'flex', alignItems: 'start' }}>
             <FlexGroup gutter="2px" prefix={<BdIcon nomargin color="deepSea50" name="icon-pin" />}>
-              <Typography variant="subtitle2" color="stone90">{formattedReportedCases}</Typography>
+              <Typography variant="subtitle2" color="stone90">
+                {formattedReportedCases}
+              </Typography>
             </FlexGroup>
           </div>
         </Grid.Column>
         <Grid.Column>
           <div sx={{ mb: '9px' }}>
-            <Typography variant="caption" color="deepSea50">Projected number of infected travellers/month</Typography>
+            <Typography variant="caption" color="deepSea50">
+              Projected number of infected travellers/month
+            </Typography>
           </div>
           <div sx={{ display: 'flex', alignItems: 'start' }}>
-            <FlexGroup gutter="2px" prefix={importationRisk
-              ? <BdIcon nomargin color="deepSea50" name="icon-plane-arrival" />
-              : <BdIcon nomargin color="deepSea50" name="icon-plane-departure" />
-            }>
-              <Typography variant="subtitle2" color="stone90">{travellers}</Typography>
+            <FlexGroup
+              gutter="2px"
+              prefix={
+                importationRisk ? (
+                  <BdIcon nomargin color="deepSea50" name="icon-plane-arrival" />
+                ) : (
+                  <BdIcon nomargin color="deepSea50" name="icon-plane-departure" />
+                )
+              }
+            >
+              <Typography variant="subtitle2" color="stone90">
+                {travellers}
+              </Typography>
             </FlexGroup>
           </div>
         </Grid.Column>
