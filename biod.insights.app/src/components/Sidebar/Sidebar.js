9f70252e458b2616fc9585bae180af8e0b0e552d
Sonya Karamchandani <sonya.karamchandani@bluedot.global>
2020-01-21 17:47:22 -0500

    wip on sidebar drawer styling and behaviour

--- biod.insights.app/src/components/Sidebar/Sidebar.js
+++ biod.insights.app/src/components/Sidebar/Sidebar.js
@@ -5,6 +5,7 @@ import { SidebarView } from 'components/SidebarView';
 import { Image, Icon } from 'semantic-ui-react';
 import { BdIcon } from 'components/_common/BdIcon';
 import { FlexGroup } from 'components/_common/FlexGroup';
+import { valignHackTop } from 'utils/cssHelpers';
 
 const Sidebar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
@@ -42,21 +43,37 @@ const Sidebar = () => {
             bg: t => t.colors.seaweed80,
             transition: '0.5s all'
           },
-          '&:focus': {
-          }
+          '&:focus': {}
         }}
       >
-        <FlexGroup gutter='2px'
+        <FlexGroup
+          gutter="2px"
+          alignItems="flex-start"
           prefix={
-            <BdIcon name="icon-chevron-left" color='white' bold 
-              sx={{
-                "&.icon.bd-icon": {
-                  fontSize: 'xx-small'
-                }
-              }} />
+            isCollapsed ? (
+              <BdIcon
+                name="icon-chevron-right"
+                color="white"
+                bold
+                sx={{
+                  '&.icon.bd-icon': { fontSize: 'xx-small'},
+                  // '&.icon.icon.bd-icon': {...valignHackTop('-1px')}
+                }}
+              />
+            ) : (
+              <BdIcon
+                name="icon-chevron-left"
+                color="white"
+                bold
+                sx={{
+                  '&.icon.bd-icon': { fontSize: 'xx-small'},
+                  // '&.icon.icon.bd-icon': {...valignHackTop('-1px')}
+                }}
+              />
+            )
           }
         >
-          <BdIcon name="icon-panels" color='white' />
+          <BdIcon name="icon-panels" color="white" />
         </FlexGroup>
       </Icon.Group>
     </div>
