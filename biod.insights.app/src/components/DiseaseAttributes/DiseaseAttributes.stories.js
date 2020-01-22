d2940487745f5d5e4eb723cade17beabe544a58c
bluedot-mtemkine <mikhail.temkine@bluedot.global>
2020-01-22 13:07:10 -0500

    MT - relocate mock data, fix EventListPanel.stories

--- biod.insights.app/src/components/DiseaseAttributes/DiseaseAttributes.stories.js
+++ biod.insights.app/src/components/DiseaseAttributes/DiseaseAttributes.stories.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import DiseaseAttributes from './DiseaseAttributes';
+import { diseaseInformation } from '__mocks__/dtoSamples';
 
 export default {
   title: 'DiseaseEventList/DiseaseAttributes'
-};
-
-// TODO: c8c632ef: move mock data to common file
-const diseaseInformation = {
-  agents: 'Bacillus anthracis',
-  agentType: 'Bacteria',
-  transmissionModes: 'Airborne, Zoonotic Fluid Transmission',
-  incubationPeriod: '?',
-  preventionMeasure: 'Vaccine',
-  biosecurityRisk:
-    'Category A: High mortality rate, easily disseminated or transmitted from person to person.'
 };
 
 export const text = () => (
