cd028216a8b77dff16f55917070164ac24640d11
Sonya Karamchandani <sonya.karamchandani@bluedot.global>
2020-01-31 15:54:50 +0000

    Merged in bugfix/PT-819-insights-at-risk-destinations (pull request #402)
    Updated display text for airports for ROI/ROE
    
    Approved-by: Kevin Chu

--- biod.insights.app/src/components/SidebarView/EventDetailPanel/EventDetailPanelDisplay.js
+++ biod.insights.app/src/components/SidebarView/EventDetailPanel/EventDetailPanelDisplay.js
@@ -144,7 +144,7 @@ const EventDetailPanelDisplay = ({
             )}
 
             {/* change to "Airports Globally with >1% Risk of Importation" */}
-            <SectionHeader>Airports with Risk of Importation >1%</SectionHeader>
+            <SectionHeader>Airports Globally with >1% Risk of Importation</SectionHeader>
             <ListLabelsHeader
               lhs={['Destination airport']}
               rhs={['Likelihood of case importation', 'Estimated case importations']}
@@ -171,7 +171,7 @@ const EventDetailPanelDisplay = ({
                 <RiskOfExportation risk={exportationRisk} />
               </Card>
 
-              <SectionHeader>Airports with Risk of Exportation >1%</SectionHeader>
+              <SectionHeader>Airports with >1% likelihood of use from event location(s)</SectionHeader>
               <ListLabelsHeader
                 lhs={['Source airport']}
                 rhs={['Global outbound vol. this month']}
