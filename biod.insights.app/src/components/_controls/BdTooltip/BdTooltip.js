5917867f4d7ecc10c725be64e6544cbcbc20bdbc
bluedot-mtemkine <mikhail.temkine@bluedot.global>
2020-02-03 15:29:09 -0500

    MT - fix tooltip displacement when close to edge of scrollable container

--- biod.insights.app/src/components/_controls/BdTooltip/BdTooltip.js
+++ biod.insights.app/src/components/_controls/BdTooltip/BdTooltip.js
@@ -16,6 +16,7 @@ const BdTooltip = ({
       wide={wide}
       position='top center'
       trigger={<span>{children}</span>}
+      popperModifiers={{ preventOverflow: { boundariesElement: "window" } }} // NOTE: this will prevent constraining of tooltips by the closest overflow:auto parent. (LINK: https://github.com/Semantic-Org/Semantic-UI-React/issues/3687#issuecomment-508046784)
     >
       <Popup.Content>
         {customPopup || text && <Typography variant="caption" color="stone10">{text}</Typography>}
