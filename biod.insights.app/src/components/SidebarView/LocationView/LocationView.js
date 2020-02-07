SonyaKaramchandani/pokemon-guide-manager

commit 9ea285f11bce61140e7a0a2b890b6a691bce0b4c

Clear panel/map display when currently selected location is deleted
Part of PT-957
Merged in bugfix/PT-957-delete-current-location (pull request #430)
Approved-by: Sonya Karamchandani
Approved-by: Kevin Chu

biod.insights.app/src/components/SidebarView/LocationView/LocationView.js
@@ -24,6 +24,7 @@ const initialState = {
 };
 
 const LOCATION_SELECTED = 'LOCATION_SELECTED';
+const LOCATION_CLEARED = 'LOCATION_CLEARED';
 const DISEASE_LIST_PANEL_CLOSED = 'DISEASE_LIST_PANEL_CLOSED';
 const DISEASE_SELECTED = 'DISEASE_SELECTED';
 const DISEASE_EVENT_LIST_PANEL_CLOSED = 'DISEASE_EVENT_LIST_PANEL_CLOSED';
@@ -40,7 +41,7 @@ const reducer = (state, action) => {
       return {
         ...state,
         geonameId: action.payload.geonameId,
-        isDiseaseListPanelVisible: true,
+        isDiseaseListPanelVisible: !!action.payload.geonameId,
         isDiseaseEventListPanelVisible: false,
         isDiseaseListPanelMinimized: false,
         isEventDetailPanelVisible: false,
@@ -66,6 +67,7 @@ const reducer = (state, action) => {
         isLocationListPanelMinimized: true,
         isDiseaseListPanelMinimized: true
       };
+    case LOCATION_CLEARED:
     case DISEASE_LIST_PANEL_CLOSED:
       return {
         ...state,
@@ -187,6 +189,10 @@ const LocationView = ({ onViewChange }) => {
     dispatch({ type: EVENT_DETAIL_PANEL_MINIMIZED, payload: value });
   };
 
+  const handleLocationListOnClear = () => {
+    dispatch({ type: LOCATION_CLEARED });
+  };
+
   const showOutbreakExtent = eventsList => {
     const eventLocations = eventsList.reduce((a, b) => [...a, ...b.eventLocations], []);
     esriMap.showEventDetailView({ eventLocations });
@@ -204,6 +210,7 @@ const LocationView = ({ onViewChange }) => {
         geonameId={state.geonameId}
         onViewChange={onViewChange}
         onSelect={handleLocationListOnSelect}
+        onClear={handleLocationListOnClear}
         isMinimized={state.isLocationListPanelMinimized}
         onMinimize={handleLocationListOnMinimize}
       />

biod.insights.app/src/components/SidebarView/LocationView/LocationListPanel/LocationListPanelContainer.js
@@ -13,13 +13,16 @@ import { LocationListSortOptions as sortOptions } from 'components/SidebarView/S
 
 import { LocationListPanelDisplay } from './LocationListPanel';
 
-function LocationListPanelContainer({ geonameId, isMinimized, onMinimize, onSelect }) {
+function LocationListPanelContainer({ geonameId, isMinimized, onMinimize, onSelect, onClear }) {
   const [geonames, setGeonames] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [sortBy, setSortBy] = useState(sortOptions[0].value);
   const [hasError, setHasError] = useState(false);
 
   const handleOnDelete = ({ data: { geonames } }) => {
+    if (!new Set(geonames.map(n => n.geonameId)).has(geonameId)) {
+      onClear();
+    }
     setGeonames(geonames);
   };
 

commit 5c57598f0c4baa76783bb6e0b5cc9be3e9a25e3b

Fix issue with disease panel not showing up when global view is selected
This was caused by commit 9ea285f11bce61140e7a0a2b890b6a691bce0b4c. A state (`LOCATION_CLEARED`) was introduced so this fix reverts the change done for `LOCATION_SELECTED` state.

Part of PT-957
Merged in bugfix/PT-957-global-view (pull request #442)
Approved-by: Gursharn Singh

biod.insights.app/src/components/SidebarView/LocationView/LocationView.js
@@ -41,7 +41,7 @@ const reducer = (state, action) => {
       return {
         ...state,
         geonameId: action.payload.geonameId,
-        isDiseaseListPanelVisible: !!action.payload.geonameId,
+        isDiseaseListPanelVisible: true,
         isDiseaseEventListPanelVisible: false,
         isDiseaseListPanelMinimized: false,
         isEventDetailPanelVisible: false,