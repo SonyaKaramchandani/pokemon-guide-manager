d65cf7a332fd35bc8f68a1495e3b6e2ccd604bc7
Mikhail Temkine <mikhail.temkine@bluedot.global>
2020-02-14 23:18:11 +0000

    MT - PT-1061 - show active route in mobile menu
    * MT - PT-1061 - show active route in mobile menu
    * MT/KC - fix routerLink navigation in non-mobile menu
    
    Merged in feature/PT-1061-show-active-route-link-in-mobile (pull request #470)
    
    Approved-by: Kevin Chu

--- biod.insights.app/src/components/Navigationbar/Navigationbar.js
+++ biod.insights.app/src/components/Navigationbar/Navigationbar.js
@@ -1,11 +1,11 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import logoSvg from 'assets/logo.svg';
 import config from 'config';
 import { Menu, Dropdown, Image } from 'semantic-ui-react';
-import { navigate } from '@reach/router';
+import { navigate, Location, globalHistory } from '@reach/router';
 import classNames from 'classnames';
 
 import { Typography } from 'components/_common/Typography';
@@ -30,6 +30,14 @@ export const Navigationbar = ({ urls }) => {
   const userProfile = useContext(UserContext);
   const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
 
+  useEffect(
+    () =>
+      globalHistory.listen(historyEvent => {
+        setIsMobileMenuVisible(false);
+      }),
+    []
+  );
+
   const _urls = [
     {
       mobile: true,
@@ -37,17 +45,11 @@ export const Navigationbar = ({ urls }) => {
       children: [
         {
           title: 'Show by Events',
-          onClick: () =>
-            navigate('/event').then(() => {
-              docCookies.setItem(CookieKeys.PREF_MAIN_PAGE, '/event', Infinity);
-            })
+          routerLink: '/event'
         },
         {
           title: 'Show by My Locations',
-          onClick: () =>
-            navigate('/location').then(() => {
-              docCookies.setItem(CookieKeys.PREF_MAIN_PAGE, '/location', Infinity);
-            })
+          routerLink: '/location'
         }
       ]
     },
@@ -117,97 +119,103 @@ export const Navigationbar = ({ urls }) => {
   // filter out undefined (unauthorized) menu items
   urls = urls.filter(u => !!u);
 
-  const nonMobileNavigationItems = urls.map(({ url, onClick, title, children, header }) => {
-    if (!children) {
-      return (
-        <Menu.Item
-          onClick={onClick ? onClick : null}
-          href={url ? parseUrl(url) : null}
-          key={header + title}
-        >
-          <Typography
-            variant="body2"
-            color="white"
-            inline
-            sx={{
-              // marginBottom: '-2px', // css hack dur to offset
-              borderBottom: '1px solid transparent',
-              ':hover': {
-                color: t => t.colors.sea30,
-                borderBottom: t => `1px solid ${t.colors.sea30}`
-              }
-            }}
+  const nonMobileNavigationItems = urls.map(
+    ({ url, onClick, routerLink, title, children, header }) => {
+      if (!children) {
+        return (
+          <Menu.Item
+            onClick={() => handleNavItemClick(routerLink, onClick, url)}
+            href={url ? parseUrl(url) : null}
+            key={header + title}
           >
-            {title}
-          </Typography>
-        </Menu.Item>
-      );
-    } else {
-      return (
-        <Dropdown
-          icon={
-            <BdIcon
-              name="icon-chevron-down"
-              sx={{ '&.icon.bd-icon': { fontWeight: 'bold', color: 'white' } }}
-            />
-          }
-          item
-          scrolling
-          trigger={
             <Typography
               variant="body2"
               color="white"
               inline
               sx={{
+                // marginBottom: '-2px', // css hack dur to offset
+                borderBottom: '1px solid transparent',
                 ':hover': {
                   color: t => t.colors.sea30,
-                  borderBottom: t => `1px solid ${t.colors.sea30}`,
-                  ...valignHackTop('1px')
+                  borderBottom: t => `1px solid ${t.colors.sea30}`
                 }
               }}
             >
               {title}
             </Typography>
-          }
-          key={header + title}
-        >
-          <Dropdown.Menu>
-            {children.map(({ divider, url, title, onClick, header }) => {
-              if (divider) {
-                return <Dropdown.Divider key={title} />;
-              }
-              if (header) {
+          </Menu.Item>
+        );
+      } else {
+        return (
+          <Dropdown
+            icon={
+              <BdIcon
+                name="icon-chevron-down"
+                sx={{ '&.icon.bd-icon': { fontWeight: 'bold', color: 'white' } }}
+              />
+            }
+            item
+            scrolling
+            trigger={
+              <Typography
+                variant="body2"
+                color="white"
+                inline
+                sx={{
+                  ':hover': {
+                    color: t => t.colors.sea30,
+                    borderBottom: t => `1px solid ${t.colors.sea30}`,
+                    ...valignHackTop('1px')
+                  }
+                }}
+              >
+                {title}
+              </Typography>
+            }
+            key={header + title}
+          >
+            <Dropdown.Menu>
+              {children.map(({ divider, url, title, onClick, routerLink, header }) => {
+                if (divider) {
+                  return <Dropdown.Divider key={title} />;
+                }
+                if (header) {
+                  return (
+                    <Dropdown.Header key={title}>
+                      <Typography variant="overline" color="white" inline>
+                        {title}
+                      </Typography>
+                    </Dropdown.Header>
+                  );
+                }
                 return (
-                  <Dropdown.Header key={title}>
-                    <Typography variant="overline" color="white" inline>
-                      {title}
-                    </Typography>
-                  </Dropdown.Header>
+                  <Dropdown.Item
+                    onClick={() => handleNavItemClick(routerLink, onClick, url)}
+                    href={url ? parseUrl(url) : null}
+                    key={title}
+                  >
+                    {title}
+                  </Dropdown.Item>
                 );
-              }
-              return (
-                <Dropdown.Item
-                  onClick={onClick ? onClick : null}
-                  href={url ? parseUrl(url) : null}
-                  key={title}
-                >
-                  {title}
-                </Dropdown.Item>
-              );
-            })}
-          </Dropdown.Menu>
-        </Dropdown>
-      );
+              })}
+            </Dropdown.Menu>
+          </Dropdown>
+        );
+      }
     }
-  });
+  );
 
   const handleOnMobileMenuToggle = () => {
     setIsMobileMenuVisible(!isMobileMenuVisible);
   };
 
-  const handleMobileNavItemClick = (onClick, url) => {
-    setIsMobileMenuVisible(false);
+  const handleNavItemClick = (routerLink, onClick, url) => {
     if (onClick) onClick();
+    if (routerLink) {
+      navigate(routerLink).then(() => {
+        docCookies.setItem(CookieKeys.PREF_MAIN_PAGE, routerLink, Infinity);
+      });
+    }
     if (url) {
       window.location.href = parseUrl(url);
     }
@@ -244,34 +252,44 @@ export const Navigationbar = ({ urls }) => {
         )}
       </Menu>
       {isMobileMenuVisible && (
-        <div className="mobile-menu-expanded">
-          {urls.map(({ mobile, url, onClick, title, children, header }) => (
-            <div
-              key={header + title}
-              className="mobile-menu-item"
-              onClick={!children ? () => handleMobileNavItemClick(onClick, url) : undefined}
-            >
-              <Typography variant="h1" color="white" inline>
-                {title}
-              </Typography>
-              {children && (
-                <div className="submenu">
-                  {children.map(({ url, title, onClick }) => (
-                    <div
-                      key={title}
-                      className="mobile-menu-item-sub"
-                      onClick={() => handleMobileNavItemClick(onClick, url)}
-                    >
-                      <Typography variant="body1" color="white" inline>
-                        {title}
-                      </Typography>
+        <Location>
+          {({ location }) => (
+            <div className="mobile-menu-expanded">
+              {urls.map(({ title, header, routerLink, onClick, url, children }) => (
+                <div
+                  key={header + title}
+                  className="mobile-menu-item"
+                  onClick={
+                    !children ? () => handleNavItemClick(routerLink, onClick, url) : undefined
+                  }
+                >
+                  <Typography variant="h1" color="white" inline>
+                    {title}
+                  </Typography>
+                  {children && (
+                    <div className="submenu">
+                      {children.map(({ title, routerLink, onClick, url }) => (
+                        <div
+                          key={title}
+                          className="mobile-menu-item-sub"
+                          onClick={() => handleNavItemClick(routerLink, onClick, url)}
+                        >
+                          <Typography
+                            variant={routerLink == location.pathname ? 'h3' : 'body1'}
+                            color="white"
+                            inline
+                          >
+                            {title}
+                          </Typography>
+                        </div>
+                      ))}
                     </div>
-                  ))}
+                  )}
                 </div>
-              )}
+              ))}
             </div>
-          ))}
-        </div>
+          )}
+        </Location>
       )}
     </div>
   );
