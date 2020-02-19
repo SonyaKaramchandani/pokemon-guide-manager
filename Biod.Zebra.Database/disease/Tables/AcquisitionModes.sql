f03185d61917074b46c8876a6298bb5591670964
Predrag Ostojic <predrag.ostojic@bluedot.global>
2020-02-19 16:53:40 +0000

    Merged in feature/PT-572-add-acquisition-mode-table (pull request #472)
    Add AcquisitionMode table
    
    PT-572
    
    Approved-by: Vivian Hu <vivian.hu@bluedot.global>
    Approved-by: Kevin Chu <kevin.chu@bluedot.global>

---
+++ Biod.Zebra.Database/disease/Tables/AcquisitionModes.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [disease].[AcquisitionModes](
+	[AcquisitionModeId] [int] NOT NULL,
+	[DiseaseVectorId] [int] NOT NULL,
+	[TransferModalityId] [int] NOT NULL,
+	[Multiplier] [int] NULL,
+	[IsDirect] [bit] NULL,
+	[AcquisitionModeLabel] [nvarchar](100) NULL,
+	[AcquisitionModeDefinitionLabel] [nvarchar](300) NULL,
+ CONSTRAINT [PK_AcquisitionModes] PRIMARY KEY CLUSTERED 
+(
+	[AcquisitionModeId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
---
+++ Biod.Zebra.Database/disease/Tables/Xtbl_Disease_AcquisitionMode.sql
@@ -0,0 +1,41 @@
+﻿CREATE TABLE [disease].[Xtbl_Disease_AcquisitionMode](
+	[DiseaseId] [int] NOT NULL,
+	[SpeciesId] [int] NOT NULL,
+	[AcquisitionModeId] [int] NOT NULL,
+	[AcquisitionModeRank] [int] NOT NULL,
+ CONSTRAINT [PK_Xtbl_Disease_AcquisitionMode] PRIMARY KEY CLUSTERED 
+(
+	[DiseaseId] ASC,
+	[SpeciesId] ASC,
+	[AcquisitionModeId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [disease].[Xtbl_Disease_AcquisitionMode] ADD  DEFAULT ((1)) FOR [SpeciesId]
+GO
+
+ALTER TABLE [disease].[Xtbl_Disease_AcquisitionMode]  WITH CHECK ADD  CONSTRAINT [FK_Xtbl_Disease_AcquisitionMode_AcquisitionModeId] FOREIGN KEY([AcquisitionModeId])
+REFERENCES [disease].[AcquisitionModes] ([AcquisitionModeId])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [disease].[Xtbl_Disease_AcquisitionMode] CHECK CONSTRAINT [FK_Xtbl_Disease_AcquisitionMode_AcquisitionModeId]
+GO
+
+ALTER TABLE [disease].[Xtbl_Disease_AcquisitionMode]  WITH CHECK ADD  CONSTRAINT [FK_Xtbl_Disease_AcquisitionMode_DiseaseId] FOREIGN KEY([DiseaseId])
+REFERENCES [disease].[Diseases] ([DiseaseId])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [disease].[Xtbl_Disease_AcquisitionMode] CHECK CONSTRAINT [FK_Xtbl_Disease_AcquisitionMode_DiseaseId]
+GO
+
+ALTER TABLE [disease].[Xtbl_Disease_AcquisitionMode]  WITH CHECK ADD  CONSTRAINT [FK_Xtbl_Disease_AcquisitionMode_SpeciesId] FOREIGN KEY([SpeciesId])
+REFERENCES [disease].[Species] ([SpeciesId])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [disease].[Xtbl_Disease_AcquisitionMode] CHECK CONSTRAINT [FK_Xtbl_Disease_AcquisitionMode_SpeciesId]
+GO
+
