04cb44cad4a17f38c2a8fa5f79c388f70f5d4c22
Gursharn Singh <Gursharn.Singh@bluedot.global>
2020-02-24 09:49:39 -0500

    Az Function email renderer using mjml & handlebars

---
+++ biod.insights.notification.emailrenderer/EmailRenderer/index.js
@@ -0,0 +1,42 @@
+const fs = require("fs").promises;
+const mjml2html = require("mjml");
+const { compile } = require("handlebars");
+
+const mjmlOptions = {};
+const emailsFolder = "emails";
+
+module.exports = async function(context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
+
+  const emailName = req.query.name || (req.body && req.body.name);
+  const reqData = req.query.data || (req.body && req.body.data);
+  const data = (reqData && JSON.parse(reqData)) || {};
+
+  context.log(`Loading mjml for email ${emailName}`);
+  const emailContent = await fs.readFile(
+    __dirname + `/${emailsFolder}/${emailName.toLowerCase()}.mjml`
+  );
+
+  context.log(`Loaded mjml for ${emailName}. Compiling mjml template.`);
+  const template = compile(emailContent.toString());
+
+  context.log(`Injecting data into ${emailName} mjml.`);
+  const htmlOutput = mjml2html(template(data), mjmlOptions);
+
+  if (emailName) {
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: htmlOutput.html,
+      headers: {
+        "Content-Type": "text/html"
+      },
+      isRaw: true
+    };
+  } else {
+    context.res = {
+      status: 400,
+      body:
+        "Please pass an email name on the query string or in the request body"
+    };
+  }
+};
