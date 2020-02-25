f5800f20413621a29f799686304650549eb27e8e
Gursharn Singh <Gursharn.Singh@bluedot.global>
2020-02-25 12:29:50 -0500

    analytics added integration test

--- biod.insights.notification.emailrenderer/EmailRenderer/analytics/index.test.js
+++ biod.insights.notification.emailrenderer/EmailRenderer/analytics/index.test.js
@@ -6,4 +6,28 @@ describe("analytics", () => {
 
     expect(html).toBe("");
   });
+
+  test("generate html", () => {
+    const emailName = "a-test-email";
+    const UserId = "UID";
+    const EventId = "EID";
+    const SentDate = "SENT-DATE";
+    const Email = "a@b.com";
+
+    const html = analyticsHtml(
+      {
+        UserId,
+        EventId,
+        SentDate,
+        Email
+      },
+      emailName
+    );
+
+    expect(html).toContain(emailName);
+    expect(html).toContain(EventId);
+    expect(html).toContain(SentDate);
+    expect(html).toContain(SentDate);
+    expect(html).toContain(Email);
+  });
 });
