208fb14858b9ba9d42ebc61daff5a9e08d0c4e50
Sonya Karamchandani <sonya.karamchandani@bluedot.global>
2020-03-02 15:58:05 +0000

    Merged in feature/PT-1065-updated-email-for-notification-stratification (pull request #510)
    Format new event activity email
    
    * format new event email, data model updates
    -updated formatting of new event email with injected data
    -changes to common files
    -added new helper functions for string formatting
    -added unit tests
    -updates to new event data model
    
    Outstanding issues will be resolved in PT-1106
    
    Approved-by: Anna Caldwell

--- biod.insights.notification.emailrenderer/EmailRenderer/utils/handlebarsUtils.js
+++ biod.insights.notification.emailrenderer/EmailRenderer/utils/handlebarsUtils.js
@@ -13,6 +13,14 @@ const singleDigitNumbers = {
   9: "Nine"
 };
 
+const OutbreakPotentialCategory = {
+  Sustained: 1,
+  Sporadic: 2,
+  NeedsMapSustained: 3,
+  NeedsMapUnlikely: 4,
+  Unlikely: 5,
+  Unknown: 6
+};
 /**
  *
  * @param {Array | Number} number
@@ -43,6 +51,66 @@ function formatNumber(number) {
   }
 }
 
+/**
+ * Formats the min to max values to an interval display text
+ * @param {Number} number A number to be represented as either a word or digit
+ * @return {String} Formatted interval string representation
+ */
+function formatPercentRange(minValue, maxValue) {
+  return (minValue * 100).toString() + "-" + (maxValue * 100).toString() + "%";
+}
+
+/**
+ * Returns the additional records that exist given the total and the returned records
+ * @param {Number} totalRecords
+ * Number of total records that exist
+ * @param {Array} records
+ * Number of records returned
+ * @return {Number} Number of additional records that exist
+ */
+function numAdditionalRecords(totalRecords, records) {
+  return totalRecords - records.length;
+}
+
+/**
+ * Returns the messaging for the outbreak potentials
+ * @param {Number} outbreakPotentialCategoryId
+ * Outbreak potential Id
+ * @return {String} Outbreak potential messaging
+ */
+
+const outbreakPotentialMsg = outbreakPotentialCategoryId =>
+  outbreakPotentialCategoryId === OutbreakPotentialCategory.Sustained ||
+  outbreakPotentialCategoryId === OutbreakPotentialCategory.NeedsMapSustained
+    ? "Potential for sustained local transmission"
+    : outbreakPotentialCategoryId === OutbreakPotentialCategory.Sporadic
+    ? "Potential for sporadic local transmission"
+    : outbreakPotentialCategoryId === OutbreakPotentialCategory.Unlikely ||
+      outbreakPotentialCategoryId === OutbreakPotentialCategory.NeedsMapUnlikely
+    ? "Potential for local transmission unlikely"
+    : outbreakPotentialCategoryId === OutbreakPotentialCategory.Unknown
+    ? "Unknown potential for local transmission"
+    : "";
+
+/**
+ * Formats the min to max values to the traveller interval display text
+ * @param {Number} minval The min importation risk expressed as a decimal
+ * @param {Number} maxval The max importation risk expressed as a decimal
+ * @param {Boolean} isModelNotRun Whether the model is not run
+ * * @return {String} Formatted interval string representation
+ */
+
+function formatImportationRisk(minVal, maxVal, isModelNotRun) {
+  if (isModelNotRun || minVal > maxVal) {
+    return "Not calculated";
+  }
+  if (maxVal <= 0) {
+    return "Negligible";
+  } else {
+    return (minVal * 100).toString() + "-" + (maxVal * 100).toString() + "%";
+  }
+}
+
 Handlebars.registerHelper(
   "gaURIComponent",
   (...params) => new Handlebars.SafeString(gaURIComponent(...params))
@@ -50,9 +118,17 @@ Handlebars.registerHelper(
 
 Handlebars.registerHelper("pluralize", pluralize);
 Handlebars.registerHelper("formatNumber", formatNumber);
+Handlebars.registerHelper("formatPercentRange", formatPercentRange);
+Handlebars.registerHelper("numAdditionalRecords", numAdditionalRecords);
+Handlebars.registerHelper("outbreakPotentialMsg", outbreakPotentialMsg);
+Handlebars.registerHelper("formatImportationRisk", formatImportationRisk);
 
 module.exports = {
   analyticsHtml,
   pluralize,
-  formatNumber
+  formatNumber,
+  formatPercentRange,
+  numAdditionalRecords,
+  outbreakPotentialMsg,
+  formatImportationRisk
 };
--- biod.insights.notification.emailrenderer/EmailRenderer/utils/handlebarsUtils.test.js
+++ biod.insights.notification.emailrenderer/EmailRenderer/utils/handlebarsUtils.test.js
@@ -1,26 +1,97 @@
-const { pluralize, formatNumber } = require("./handlebarsUtils");
+const handlebarsUtils = require("./handlebarsUtils");
 
 const singleArray = ["Lao People's Democratic Republic, Laos"];
 
 const multipleArray = ["ProMed", "News Media", "WHO", "CDC"];
 
+const userLocations = [
+  {
+    locationType: 2,
+    geonameId: 2984294,
+    locationName: "Lao People's Democratic Republic, Laos",
+    isLocal: true,
+    outbreakPotentialCategoryId: 2,
+    importationRisk: {
+      isModelNotRun: true,
+      minProbability: 0.05,
+      maxProbability: 0.27
+    }
+  },
+  {
+    locationType: 2,
+    geonameId: 2984294,
+    locationName: "Lao People's Democratic Republic, Laos",
+    isLocal: true,
+    outbreakPotentialCategoryId: 2,
+    importationRisk: {
+      isModelNotRun: true,
+      minProbability: 0.5,
+      maxProbability: 0.7
+    }
+  },
+  {
+    locationType: 2,
+    geonameId: 2984294,
+    locationName: "Toronto, Ontario, Canada",
+    isLocal: true,
+    outbreakPotentialCategoryId: 2,
+    importationRisk: {
+      isModelNotRun: true,
+      minProbability: 0.09,
+      maxProbability: 0.1
+    }
+  }
+];
+
 describe("handlebarsUtils", () => {
   test("plurality of 1 number", () => {
-    expect(pluralize(1, "case", "cases")).toBe("case");
+    expect(handlebarsUtils.pluralize(1, "case", "cases")).toBe("case");
   });
   test("plurality for more than 1 number", () => {
-    expect(pluralize(23, "disease", "diseases")).toBe("diseases");
+    expect(handlebarsUtils.pluralize(23, "disease", "diseases")).toBe(
+      "diseases"
+    );
   });
   test("plurality of 1 array item", () => {
-    expect(pluralize(singleArray, "location", "locations")).toBe("location");
+    expect(
+      handlebarsUtils.pluralize(singleArray, "location", "locations")
+    ).toBe("location");
   });
   test("plurality of more than 1 array item", () => {
-    expect(pluralize(multipleArray, "source", "sources")).toBe("sources");
+    expect(handlebarsUtils.pluralize(multipleArray, "source", "sources")).toBe(
+      "sources"
+    );
   });
   test("format number as word", () => {
-    expect(formatNumber(2)).toBe("Two");
+    expect(handlebarsUtils.formatNumber(2)).toBe("Two");
   });
   test("format number as digit", () => {
-    expect(formatNumber(34)).toBe(34);
+    expect(handlebarsUtils.formatNumber(34)).toBe(34);
+  });
+  test("format percent range", () => {
+    expect(handlebarsUtils.formatPercentRange(0.13, 0.46)).toBe("13-46%");
+  });
+  test("return additional records", () => {
+    expect(handlebarsUtils.numAdditionalRecords(43, userLocations)).toBe(40);
+  });
+  test("outbreak potential messaging", () => {
+    expect(handlebarsUtils.outbreakPotentialMsg(3)).toBe(
+      "Potential for sustained local transmission"
+    );
+  });
+  test("return formatted percent range", () => {
+    expect(handlebarsUtils.formatImportationRisk(0.13, 0.46, false)).toBe(
+      "13-46%"
+    );
+  });
+  test("return not calculated messaging", () => {
+    expect(handlebarsUtils.formatImportationRisk(0.13, 0.46, true)).toBe(
+      "Not calculated"
+    );
+  });
+  test("return negligible messaging", () => {
+    expect(handlebarsUtils.formatImportationRisk(0, 0, false)).toBe(
+      "Negligible"
+    );
   });
 });
