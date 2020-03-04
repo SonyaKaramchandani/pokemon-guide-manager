97489e605f11ad2712d4e5dab41611a76bbfc0ce
Mikhail Temkine <mikhail.temkine@bluedot.global>
2020-03-04 20:50:11 +0000

    Temp fix for overflowing tooltip on mobile
    * MT - PT-1121 - temp fix: use responsive utilities to disable popperjs preventOverflow/window
    * MT - update popup triggers
    * MT/KC - comment out hide Popperjs modifier for now
    
    Merged in hotfix/PT-1121-tooltip-continues-to-show-upon-e (pull request #516)
    Approved-by: Kevin Chu

--- biod.insights.app/src/components/_controls/BdTooltip/BdTooltip.js
+++ biod.insights.app/src/components/_controls/BdTooltip/BdTooltip.js
@@ -1,21 +1,29 @@
 /** @jsx jsx */
 import React from 'react';
 import { jsx } from 'theme-ui';
+import { useBreakpointIndex } from '@theme-ui/match-media';
 import { Popup } from 'semantic-ui-react';
 import { Typography } from 'components/_common/Typography';
+import { isMobile, isNonMobile } from 'utils/responsive';
 
 const BdTooltip = ({ text, customPopup, wide, children }) => {
+  const isNonMobileDevice = isNonMobile(useBreakpointIndex());
+  const isMobileDevice = isMobile(useBreakpointIndex());
   return (
     <Popup
       // pinned open // DEBUG only!
       wide={wide}
       position="top center"
       trigger={<span>{children}</span>}
+      on={['hover', 'focus', 'click']}
       popperModifiers={{
-        preventOverflow: { boundariesElement: 'window' },
+        preventOverflow: isNonMobileDevice
+          ? { boundariesElement: 'window' } // NOTE: this will prevent constraining of tooltips by the closest overflow:auto parent. (LINK: https://github.com/Semantic-Org/Semantic-UI-React/issues/3687#issuecomment-508046784)
+          : { enabled: true },
         shift: { enabled: true },
         flip: { enabled: true },
-      }} // NOTE: this will prevent constraining of tooltips by the closest overflow:auto parent. (LINK: https://github.com/Semantic-Org/Semantic-UI-React/issues/3687#issuecomment-508046784)
+        //hide: { enabled: true }, // TODO: this should apply 'x-out-of-boundaries' attribute according to [documentation](https://popper.js.org/docs/v1/#modifiershide) but it does not.
+      }}
     >
       <Popup.Content>
         {customPopup ||
