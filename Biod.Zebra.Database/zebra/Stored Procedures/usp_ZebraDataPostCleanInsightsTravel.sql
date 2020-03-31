eab5ef53aad11360ae03f5311f9d04f708f08103
Vivian Hu <vivian.hu@bluedot.global>
2020-03-31 20:54:11 +0000

    Merged in feature/PT-1219-1225-InsightsTravelData (pull request #627)
    Feature/PT-1219 1225 InsightsTravelData
    
    * SSIS to transfer 3 datasets
    
    * Adding AirportRanking in SSIS
    
    * Merge remote-tracking branch 'origin/develop' into feature/PT-1219-1225-InsightsTravelData
    
    * SPs to clean and restore db components
    
    * Added missing schema
    
    Approved-by: Predrag Ostojic

---
+++ Biod.Zebra.Database/zebra/Stored Procedures/usp_ZebraDataPostCleanInsightsTravel.sql
@@ -0,0 +1,29 @@
+﻿
+-- =============================================
+-- Author:		Vivian
+-- Create date: 2018-09 
+-- Description:	Returns a country shape text by geonameId
+-- 2019-07 name changed
+-- =============================================
+CREATE PROCEDURE [zebra].usp_ZebraDataPostCleanInsightsTravel
+	@EventId    AS INT
+	--,@EventLocationCases nvarchar(max)
+AS
+BEGIN
+	SET NOCOUNT ON
+	BEGIN TRY
+	BEGIN TRAN
+		--add w/o check
+		ALTER TABLE [zebra].[Stations] WITH NOCHECK ADD CONSTRAINT [FK_Stations_CityGeoname] FOREIGN KEY([CityGeonameId])
+			REFERENCES [place].[Geonames] ([GeonameId])
+		ALTER TABLE [zebra].[Stations] WITH NOCHECK ADD CONSTRAINT FK_Stations_Geoname FOREIGN KEY([GeonameId])
+			REFERENCES [place].[Geonames] ([GeonameId])
+	--action!
+	COMMIT TRAN
+	END TRY
+
+	BEGIN CATCH
+		ROLLBACK TRAN
+		Select Cast (0 as bit) as Result
+	END CATCH;
+END
