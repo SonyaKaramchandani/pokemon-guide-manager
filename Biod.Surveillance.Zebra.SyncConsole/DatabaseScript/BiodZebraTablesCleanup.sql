5c1bfac92964cbe8c729a437f6f972dcaa7de2b6
Predrag Ostojic <predrag.ostojic@bluedot.global>
2020-03-31 20:57:47 +0000

    Merged in bugfix/PT-1248-do-not-update-old-model-tables (pull request #628)
    Remove obsolete history tables
    
    Approved-by: Vivian Hu <vivian.hu@bluedot.global>

--- Biod.Surveillance.Zebra.SyncConsole/DatabaseScript/BiodZebraTablesCleanup.sql
+++ Biod.Surveillance.Zebra.SyncConsole/DatabaseScript/BiodZebraTablesCleanup.sql
@@ -12,11 +12,7 @@ Truncate table [zebra].EventSourceAirport
 Truncate table [zebra].[EventPrevalence]
 Delete from [surveillance].[Event]
 delete from [surveillance].Xtbl_Event_Location_history
-delete from [zebra].[EventDestinationAirport_history]
-delete from [zebra].[EventDestinationGrid_history]
 delete from [zebra].EventImportationRisksByUser
-delete from [zebra].[EventImportationRisksByUser_history]
-delete from [zebra].[EventPrevalence_history]
 delete from [zebra].[UserAois_history]
 truncate table [zebra].[Xtbl_User_Disease_Relevance]
 truncate table [zebra].[Xtbl_Role_Disease_Relevance]
--- Biod.Zebra.Database/zebra/Stored Procedures/usp_ZebraEmailSetWeeklyData.sql
+++ Biod.Zebra.Database/zebra/Stored Procedures/usp_ZebraEmailSetWeeklyData.sql
@@ -13,10 +13,6 @@ BEGIN
 	BEGIN TRAN
 		--clean data
 		Delete from surveillance.Xtbl_Event_Location_history Where EventDateType=2
-		TRUNCATE TABLE zebra.EventPrevalence_history
-		TRUNCATE TABLE zebra.EventDestinationAirport_history
-		TRUNCATE TABLE zebra.EventDestinationGrid_history
-		TRUNCATE TABLE zebra.EventImportationRisksByUser_history
 		TRUNCATE TABLE zebra.UserAois_history
 
 		--update data
@@ -26,25 +22,6 @@ BEGIN
 			Select [EventId], 2, [GeonameId], [EventDate]
 				,[SuspCases],[ConfCases],[RepCases],[Deaths]
 			From surveillance.Xtbl_Event_Location
-
-		Insert into zebra.EventPrevalence_history(
-				[EventId], MinPrevelance, MaxPrevelance, EventMonth)
-			Select [EventId], MinPrevelance, MaxPrevelance, EventMonth
-			From zebra.EventPrevalence
-
-		Insert into zebra.EventDestinationAirport_history(
-				[EventId], DestinationStationId, Volume, MaxProb, MinProb, MaxExpVolume, MinExpVolume)
-			Select [EventId], DestinationStationId, Volume, MaxProb, MinProb, MaxExpVolume, MinExpVolume
-			From zebra.EventDestinationAirport
-
-		Insert into zebra.EventDestinationGrid_history([EventId], GridId)
-			Select [EventId], GridId
-			From zebra.EventDestinationGridV3
-
-		Insert into zebra.EventImportationRisksByUser_history(
-				UserId, [EventId], LocalSpread, MaxProb, MinProb, MaxVolume, MinVolume)
-			Select UserId, [EventId], LocalSpread, MaxProb, MinProb, MaxVolume, MinVolume
-			From zebra.EventImportationRisksByUser
 
 		Insert into zebra.UserAois_history(UserId, AoiGeonameIds)
 			Select Id, AoiGeonameIds
