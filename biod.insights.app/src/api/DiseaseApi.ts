d4ec5c1540da9d17c85bac71b3c958cd59425b7a
Mikhail Temkine <mikhail.temkine@bluedot.global>
2020-04-15 19:27:30 +0000

    PT-1200 - convert to ts: DiseaseApi
    Merged in PT-1200-ts-DiseaseApi (pull request #681)
    Approved-by: Kevin Chu

---
+++ biod.insights.app/src/api/DiseaseApi.ts
@@ -0,0 +1,51 @@
+import axios, { CancelToken } from 'client';
+import { AxiosResponse } from 'axios';
+import * as dto from 'client/dto';
+
+let getDiseaseRiskByLocationCancel = null;
+
+const headers = {
+  'X-Entity-Type': 'Disease'
+};
+
+function getDiseaseRiskByLocation(options: {
+  geonameId: number;
+}): Promise<AxiosResponse<dto.RiskAggregationModel>> {
+  const { geonameId } = options;
+  getDiseaseRiskByLocationCancel && getDiseaseRiskByLocationCancel();
+  return axios.get(`/api/diseaserisk`, {
+    params: {
+      geonameId
+    },
+    cancelToken: new CancelToken(c => (getDiseaseRiskByLocationCancel = c)),
+    headers
+  });
+}
+
+function getDisease(options: {
+  diseaseId: number;
+}): Promise<AxiosResponse<dto.DiseaseInformationModel>> {
+  const { diseaseId } = options;
+  return axios.get(`/api/disease/${diseaseId}`, { headers });
+}
+
+function getDiseaseCaseCount(options: {
+  diseaseId: number;
+  geonameId: number;
+}): Promise<AxiosResponse<dto.CaseCountModel>> {
+  const { diseaseId, geonameId } = options;
+  const url = `/api/disease/${diseaseId}/casecount`;
+
+  return axios.get(url, {
+    params: {
+      diseaseId,
+      geonameId
+    }
+  });
+}
+
+export default {
+  getDiseaseRiskByLocation,
+  getDisease,
+  getDiseaseCaseCount
+};
