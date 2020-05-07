22b7f5aff31998eb9c3d593c80244363e74af14e
Kevin Chu <kevin.chu@bluedot.global>
2020-05-07 20:11:51 +0000

    Add User Id if available to js log messages.
    Merged in hotfix/js-log-user-id (pull request #762)
    Approved-by: Mikhail Temkine <mikhail.temkine@bluedot.global>

--- Biod.Insights.Api/Controllers/LoggingController.cs
+++ Biod.Insights.Api/Controllers/LoggingController.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
+using Biod.Insights.Service.Helpers;
 using Biod.Insights.Service.Models.Logging;
+using Biod.Products.Common;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -10,35 +12,38 @@ namespace Biod.Insights.Api.Controllers
     public class LoggingController : ControllerBase
     {
         private readonly ILogger<LoggingController> _logger;
+        private readonly IEnvironmentVariables _environmentVariables;
 
-        public LoggingController(ILogger<LoggingController> logger)
+        public LoggingController(ILogger<LoggingController> logger, IEnvironmentVariables environmentVariables)
         {
             _logger = logger;
+            _environmentVariables = environmentVariables;
         }
 
         [HttpPost("js")]
         public ActionResult SaveJsLog([FromBody] [Required] JsLogModel logModel)
         {
+            var tokenUserId = ClaimsHelper.GetUserId(HttpContext.User?.Claims, _environmentVariables).DefaultIfWhiteSpace("Unknown User");
             switch (logModel.LogLevel.ToLowerInvariant())
             {
                 case "critical":
                 case "fatal":
-                    _logger.LogCritical(logModel.Message);
+                    _logger.LogCritical($"User [{tokenUserId}]: {logModel.Message}");
                     break;
                 case "error":
                 case "exception":
-                    _logger.LogError(logModel.Message);
+                    _logger.LogError($"User [{tokenUserId}]: {logModel.Message}");
                     break;
                 case "warning":
                 case "warn":
-                    _logger.LogWarning(logModel.Message);
+                    _logger.LogWarning($"User [{tokenUserId}]: {logModel.Message}");
                     break;
                 case "information":
                 case "info":
-                    _logger.LogInformation(logModel.Message);
+                    _logger.LogInformation($"User [{tokenUserId}]: {logModel.Message}");
                     break;
                 default:
-                    _logger.LogDebug(logModel.Message);
+                    _logger.LogDebug($"User [{tokenUserId}]: {logModel.Message}");
                     break;
             }
 
