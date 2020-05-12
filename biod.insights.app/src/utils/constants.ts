317e06031d061f18c1d0cd3acf9437809fd19063
Mikhail Temkine <mikhail.temkine@bluedot.global>
2020-05-12 00:45:15 +0000

    PT-1045 - re-enable settings routes
    Merged in feature/PT-1045-enable-settins-routes-again (pull request #772)
    Approved-by: Kevin Chu

--- biod.insights.app/src/utils/constants.ts
+++ biod.insights.app/src/utils/constants.ts
@@ -28,4 +28,4 @@ export type LogLevel = 'fatal' | 'error' | 'warn' | 'info' | 'debug';
 
 export const DisableTRANSPAR = false;
 export const ShowTranspar2Mode = true;
-export const DisableNewSettingsRoutes = true;
+export const DisableNewSettingsRoutes = false;
